8f4eaa8cd9bc37ac38d4e5154ce33358793b988a
Vansh Sutariya <sutariyavansh@gmail.com>
2023-08-23 16:46:20 +0530

    First Commit

---
+++ Recursion/LinkList/LL_1.java
@@ -0,0 +1,62 @@
+package LinkList;
+
+public class LL_1 {
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LL_1 list = new LL_1();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("list");
+        list.printList();
+        list.addFirst("This");
+        list.printList();
+    }
+}
